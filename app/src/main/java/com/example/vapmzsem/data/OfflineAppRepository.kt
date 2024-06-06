958f9a5279b382dc837ac0057d39ba952323f58d
Martin Kulich <kulich@stud.uniza.sk>
2024-06-07 01:23:24 +0200

    Nepekne ale pri vytvarani/upravovani Tankovania je dodrzana integrita databazy

--- app/src/main/java/com/example/vapmzsem/data/OfflineAppRepository.kt
+++ app/src/main/java/com/example/vapmzsem/data/OfflineAppRepository.kt
@@ -1,11 +1,15 @@
 package com.example.vapmzsem.data
 
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.first
 import java.util.Date
 
 class OfflineAppRepository(private val fuelingDao : FuelingDao, private val routeDao: RouteDao) : AppRepository {
     override suspend fun insert(item: Fueling) {
         fuelingDao.insert(item)
+        fixIntegrity()
     }
 
     override suspend fun insert(item: Route) {
@@ -19,6 +23,7 @@ class OfflineAppRepository(private val fuelingDao : FuelingDao, private val rout
 
     override suspend fun update(item: Fueling) {
         fuelingDao.update(item)
+        fixIntegrity()
     }
 
     override suspend fun update(item: Route) {
@@ -26,8 +31,28 @@ class OfflineAppRepository(private val fuelingDao : FuelingDao, private val rout
         val fuelingToInser = item.copy(id_F = corespondingFuelingId)
         routeDao.update(fuelingToInser)
     }
+    suspend fun updateRaw(item : Route){
+        routeDao.update(item)
+    }
 
     override suspend fun delete(item: Fueling) {
+        val routesToFueling = getAllRoutesToFueling(item.id_F).first()
+        for(route in routesToFueling){
+            val disconectedRoute = route.copy(id_F = null)
+            updateRaw(disconectedRoute)
+        }
+        fuelingDao.delete(item)
+        if (routesToFueling.isNotEmpty()) {
+            val newCorespondingFueling =
+                findCorespondingFueling(routesToFueling.first().finish_time)
+            for (route in routesToFueling) {
+                val updatedroute = route.copy(id_F = newCorespondingFueling)
+                updateRaw(updatedroute)
+            }
+        }
+
+    }
+    suspend fun deleteRaw(item: Fueling) {
         fuelingDao.delete(item)
     }
 
@@ -69,5 +94,17 @@ class OfflineAppRepository(private val fuelingDao : FuelingDao, private val rout
 
     override fun findCorespondingFueling(id: Int): Fueling? {
         return fuelingDao.findCorespondingFueling(id)
+    }
+
+    suspend fun fixIntegrity(){
+        val routes = getAllRoutes().first()
+
+        for (route in routes){
+            val fixedId_F = findCorespondingFueling(route.finish_time)
+            if(route.id_F != fixedId_F){
+                val updatedRoute = route.copy(id_F = fixedId_F)
+                updateRaw(updatedRoute)
+            }
+        }
     }
 }
